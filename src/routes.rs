555f057be89965a66dcd08b76daf94715a1be816
Loewetiger <loewetiger@protonmail.com>
2022-08-20 11:48:35 +0200

    refactor: replace hello world route with admission

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,11 @@
 //! The routes for the webserver.
 
-/// Quick hello world route to test the basic syntax.
-/// 
-/// Just always returns a `hello world` string.
-#[get("/")]
-pub fn index() -> &'static str {
-    "Hello, world!"
+use rocket::serde::json::Json;
+use rocket_db_pools::Connection;
+
+use crate::{db::Mitts, objects::{AdmissionResponse, Admission}, admission::handle_admission};
+
+#[post("/admission", data = "<adm>")]
+pub fn post_admission(adm: Json<Admission>, db: Connection<Mitts>) -> Json<AdmissionResponse> {
+    Json(handle_admission(adm.into_inner(), db))
 }
