1b4017dcdcd7faffc82f67f4faab5594d9e55afd
Loewetiger <loewetiger@protonmail.com>
2022-08-20 13:32:49 +0200

    refactor: make admission route async

--- src/routes.rs
+++ src/routes.rs
@@ -6,6 +6,6 @@ use rocket_db_pools::Connection;
 use crate::{db::Mitts, objects::{AdmissionResponse, Admission}, admission::handle_admission};
 
 #[post("/admission", data = "<adm>")]
-pub fn post_admission(adm: Json<Admission>, db: Connection<Mitts>) -> Json<AdmissionResponse> {
-    Json(handle_admission(adm.into_inner(), db))
+pub async fn post_admission(adm: Json<Admission>, db: Connection<Mitts>) -> Json<AdmissionResponse> {
+    Json(handle_admission(adm.into_inner(), db).await)
 }
