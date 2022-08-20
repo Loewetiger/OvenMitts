49c2abd49b816a14f2fc8aac351af65917a4a24b
Loewetiger <loewetiger@protonmail.com>
2022-08-20 13:33:20 +0200

    feat: finish admission logic

--- src/admission.rs
+++ src/admission.rs
@@ -2,11 +2,29 @@
 
 use rocket_db_pools::Connection;
 
-use crate::{objects::{AdmissionResponse, Admission}, db::Mitts};
+use crate::{
+    db::Mitts,
+    objects::{Admission, AdmissionResponse, User},
+};
 
-pub fn handle_admission(adm: Admission, mut db: Connection<Mitts>) -> AdmissionResponse {
-    let url = adm.borrow_url();
-    let segments = url.path_segments().map(|c| c.collect::<Vec<&str>>());
-    println!("{}", segments.unwrap()[1]);
-    AdmissionResponse::deny()
+pub async fn handle_admission(adm: Admission, mut db: Connection<Mitts>) -> AdmissionResponse {
+    let mut url = adm.borrow_url().clone();
+    let mut path = match url.path_segments().map(|c| c.collect::<Vec<_>>()) {
+        Some(vec) => vec,
+        None => return AdmissionResponse::deny(),
+    };
+    // Get the last element, which should be the stream key
+    let stream_key = path.pop().unwrap_or("");
+    let user = sqlx::query_as!(User, "SELECT * FROM users WHERE stream_key = ?", stream_key)
+        .fetch_one(&mut *db)
+        .await;
+
+    match user {
+        Ok(user) => {
+            path.push(&user.username);
+            url.set_path(&path.join("/"));
+            AdmissionResponse::allow(url)
+        }
+        Err(_) => AdmissionResponse::deny(),
+    }
 }
