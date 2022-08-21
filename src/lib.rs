acc6720024c1d58b0e7249d916f2452df5f56d65
Loewetiger <loewetiger@protonmail.com>
2022-08-21 12:52:11 +0200

    Add new modules, warn on missing documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+//! A set of tools built around OvenMediaEngine, which makes hosting your own low-latency streaming server easy.
+
+#![warn(missing_docs)]
 #[macro_use]
 extern crate rocket;
 
 mod admission;
+pub mod auth;
 pub mod db;
 pub mod objects;
+pub mod queries;
 pub mod routes;
