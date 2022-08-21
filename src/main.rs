444e7571aca56fce5ef0488bb55fa5fbc36f2edb
Loewetiger <loewetiger@protonmail.com>
2022-08-21 12:52:18 +0200

    fix: mount user route

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use ovenmitts::routes::*;
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![post_admission])
+        .mount("/", routes![post_admission, get_user])
         .attach(Mitts::init())
         .attach(AdHoc::try_on_ignite("SQLx Migrations", run_migrations))
 }
