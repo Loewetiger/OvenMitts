db3ec6d4092d1c3c56a465e4307f7690a1755ec5
Loewetiger <loewetiger@protonmail.com>
2022-08-21 23:22:22 +0200

    Use alternative crate for Argon2

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,6 +1,9 @@
 //! Password hashing, verification and random number generation
 
-use argon2::Config;
+use argon2::{
+    password_hash::{PasswordHash, PasswordHasher},
+    Argon2, PasswordVerifier,
+};
 use rand_core::{OsRng, RngCore};
 
 /// Generate a random byte array of the given length with the OS's secure random number generator.
@@ -12,13 +15,20 @@ pub fn random_data(size: usize) -> Vec<u8> {
 }
 
 /// Hash a password using Argon2id.
-pub fn hash_password(password: &[u8]) -> Result<String, argon2::Error> {
+pub fn hash_password(password: &[u8]) -> Result<String, argon2::password_hash::Error> {
     let salt = random_data(16);
-    let config = Config::default();
-    argon2::hash_encoded(password, &salt, &config)
+    let argon2 = Argon2::default();
+    let password_hash = argon2
+        .hash_password(password, &base64::encode(salt))?
+        .to_string();
+    Ok(password_hash)
 }
 
 /// Verify a password against a given hash.
-pub fn verify_password(hash: &str, password: &[u8]) -> Result<bool, argon2::Error> {
-    argon2::verify_encoded(hash, password)
+pub fn verify_password(hash: &str, password: &[u8]) -> Result<bool, argon2::password_hash::Error> {
+    let parsed_hash = PasswordHash::new(hash)?;
+    let result = Argon2::default()
+        .verify_password(password, &parsed_hash)
+        .is_ok();
+    Ok(result)
 }
