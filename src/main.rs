4419a58574f55219070e9cb94e903201e8d62481
Loewetiger <loewetiger@protonmail.com>
2022-08-21 17:14:10 +0200

    Implement permissions based on rocket-grants

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
+use ovenmitts::auth::extract_permissions;
 use rocket::fairing::AdHoc;
 use rocket_db_pools::Database;
+use rocket_grants::GrantsFairing;
 
 use ovenmitts::db::{run_migrations, Mitts};
 use ovenmitts::routes::*;
@@ -11,6 +13,9 @@ use ovenmitts::routes::*;
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![post_admission, get_user])
+        .attach(GrantsFairing::with_extractor_fn(|req| {
+            Box::pin(extract_permissions(req))
+        }))
         .attach(Mitts::init())
         .attach(AdHoc::try_on_ignite("SQLx Migrations", run_migrations))
 }
