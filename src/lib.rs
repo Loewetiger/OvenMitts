9c3a889961c7a395520e89184e61699516ad74ea
Loewetiger <loewetiger@protonmail.com>
2022-11-15 13:48:35 +0100

    feat: verify if the chosen username contains invalid characters

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 //! A set of tools built around `OvenMediaEngine`, which makes hosting your own low-latency streaming server easy.
 
 #![warn(missing_docs)]
+
+use lazy_static::lazy_static;
+use regex::Regex;
 #[macro_use]
 extern crate rocket;
 
@@ -10,3 +13,12 @@ pub mod db;
 pub mod objects;
 pub mod routes;
 pub mod static_files;
+
+lazy_static! {
+    /// A regex that matches a valid username.
+    ///
+    /// A valid username must:
+    /// - be between 4 and 25 characters long
+    /// - only contain alphanumeric characters and underscores
+    pub static ref USERNAME_RE: Regex = Regex::new("^[a-zA-Z0-9_]{4,25}$").unwrap();
+}
