8f0af2042d7f0e3a21053eb2c023bf73daa8d0fb
Loewetiger <loewetiger@protonmail.com>
2022-11-15 11:46:37 +0100

    refactor: switch to private cookies for session management

--- migrations/20220819185717_users_session.sql
+++ migrations/20220819185717_users_session.sql
@@ -1,15 +1,9 @@
 CREATE TABLE users (
-    id TEXT PRIMARY KEY NOT NULL UNIQUE,
-    username TEXT NOT NULL UNIQUE,
+    username TEXT PRIMARY KEY NOT NULL,
     display_name TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     stream_key TEXT NOT NULL UNIQUE,
     permissions TEXT,
-    stream_title TEXT
-);
-CREATE TABLE sessions (
-    session TEXT PRIMARY KEY NOT NULL UNIQUE,
-    user_id TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    stream_title TEXT,
+    UNIQUE (username COLLATE NOCASE)
 );
